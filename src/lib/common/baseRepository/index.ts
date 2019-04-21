d20b9eea427ac48b7ba185ceff86fef6099f29d3
Mieszko Wawrzyniak <kaaboaye@gmail.com>
2019-04-22 00:02:57 +0200

    remove redundent properties

--- src/lib/common/baseRepository/index.ts
+++ src/lib/common/baseRepository/index.ts
@@ -4,84 +4,79 @@ import {
   BaseRepositoryArguments,
   BaseRepositoryConnection,
   BaseRepositoryInternalOptions,
-  BaseRepositorySelector,
-  BaseRepositoryTable,
   defaultBaseRepositoryOptions,
 } from './options';
 
 export function BaseRepository<Entity, IdType = number>({
   table,
   selector,
-  opts,
+  opts: opts_,
 }: BaseRepositoryArguments) {
+  const opts: BaseRepositoryInternalOptions = opts_
+    ? {
+        id: opts_.id === undefined ? defaultBaseRepositoryOptions.id : opts_.id,
+
+        idGenerator: opts_.idGenerator,
+
+        sourceInsertedAt:
+          opts_.sourceInsertedAt === undefined
+            ? defaultBaseRepositoryOptions.sourceInsertedAt
+            : opts_.sourceInsertedAt,
+
+        sourceUpdatedAt:
+          opts_.sourceUpdatedAt === undefined
+            ? defaultBaseRepositoryOptions.sourceUpdatedAt
+            : opts_.sourceUpdatedAt,
+      }
+    : defaultBaseRepositoryOptions;
+
   return class implements BaseRepositoryInterface<Entity, IdType> {
-    public readonly table: BaseRepositoryTable = table;
-
-    protected readonly selector: BaseRepositorySelector = selector;
-
     private readonly __connection__: BaseRepositoryConnection;
-    private readonly __opts__: Readonly<BaseRepositoryInternalOptions> = opts
-      ? {
-          id: opts.id === undefined ? defaultBaseRepositoryOptions.id : opts.id,
-
-          idGenerator: opts.idGenerator,
-
-          sourceInsertedAt:
-            opts.sourceInsertedAt === undefined
-              ? defaultBaseRepositoryOptions.sourceInsertedAt
-              : opts.sourceInsertedAt,
-
-          sourceUpdatedAt:
-            opts.sourceUpdatedAt === undefined
-              ? defaultBaseRepositoryOptions.sourceUpdatedAt
-              : opts.sourceUpdatedAt,
-        }
-      : defaultBaseRepositoryOptions;
 
     constructor(connection: BaseRepositoryConnection) {
       this.__connection__ = connection;
     }
 
     public select(id: IdType): QueryBuilder {
-      let query = this.__connection__.knex.select(...this.selector).from(this.table);
+      let query = this.__connection__.knex.select(...selector).from(table);
 
-      if (typeof this.__opts__.id === 'string') {
-        query = query.where(this.__opts__.id, id as any);
+      if (typeof opts.id === 'string') {
+        query = query.where(opts.id, id as any);
       }
 
       return query;
     }
 
     public selectMany(): QueryBuilder {
-      return this.__connection__.knex.select(...this.selector).from(this.table);
+      return this.__connection__.knex.select(...selector).from(table);
     }
 
     public insert(attrs_: Partial<Entity>): QueryBuilder {
       const attrs = attrs_ as any;
 
-      if (typeof this.__opts__.id === 'string' && typeof this.__opts__.idGenerator === 'function') {
-        attrs[this.__opts__.id] = this.__opts__.idGenerator();
+      if (typeof opts.id === 'string' && typeof opts.idGenerator === 'function') {
+        attrs[opts.id] = opts.idGenerator();
       }
 
-      if (typeof this.__opts__.sourceInsertedAt === 'string') {
-        attrs[this.__opts__.sourceInsertedAt] = new Date();
+      if (typeof opts.sourceInsertedAt === 'string') {
+        attrs[opts.sourceInsertedAt] = new Date();
       }
 
-      if (typeof this.__opts__.sourceUpdatedAt === 'string') {
-        attrs[this.__opts__.sourceUpdatedAt] = new Date();
+      if (typeof opts.sourceUpdatedAt === 'string') {
+        attrs[opts.sourceUpdatedAt] = new Date();
       }
 
-      return this.__connection__.knex(this.table).insert(attrs, this.selector);
+      return this.__connection__.knex(table).insert(attrs, selector);
     }
 
     public update(id: IdType, attrs: Partial<Entity>): QueryBuilder {
-      let query = this.__connection__.knex(this.table);
+      let query = this.__connection__.knex(table);
 
-      if (typeof this.__opts__.id === 'string') {
-        query = query.where(this.__opts__.id, id as any);
+      if (typeof opts.id === 'string') {
+        query = query.where(opts.id, id as any);
       }
 
-      return query.update(attrs, this.selector);
+      return query.update(attrs, selector);
     }
   };
 }
--- src/lib/common/baseRepository/interface.ts
+++ src/lib/common/baseRepository/interface.ts
@@ -1,8 +1,6 @@
 import { QueryBuilder } from 'knex';
-import { BaseRepositoryTable } from './options';
 
 export interface BaseRepositoryInterface<Entity, IdType = number> {
-  table: BaseRepositoryTable;
   select(id: IdType): QueryBuilder;
   selectMany(): QueryBuilder;
   insert(attrs: Partial<Entity>): QueryBuilder;
